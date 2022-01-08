90df76e03b678b580b4e13d36abcb0f4e17984db
15387710701 <lhw15387710701@163.com>
2022-01-08 21:44:40 +0800

    dev配置文件

--- src/main/java/com/ms/demo/SpringbootStartDemoApplication.java
+++ src/main/java/com/ms/demo/SpringbootStartDemoApplication.java
@@ -1,8 +1,9 @@
 package com.ms.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@MapperScan("com.ms.demo.mapper")
 @SpringBootApplication
 public class SpringbootStartDemoApplication {
 
